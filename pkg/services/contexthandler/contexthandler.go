434c848fac6e2a901e228b04acc96ba7196b5567
Karl Persson <kalle.persson@grafana.com>
2023-11-09 14:43:26 +0100

    Always use same header name and add namespace to header value

--- pkg/services/contexthandler/contexthandler.go
+++ pkg/services/contexthandler/contexthandler.go
@@ -4,6 +4,7 @@ package contexthandler
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -162,12 +163,6 @@ func getNamespaceAndID(user *user.SignedInUser) (string, string) {
 	return namespace, id
 }
 
-var namespaceToHeader = map[string]string{
-	"user":            "User",
-	"api-key":         "Api-Key",
-	"service-account": "Service-Account",
-}
-
 func (h *ContextHandler) addIDHeaderEndOfRequestFunc(namespace, id string) web.BeforeFunc {
 	return func(w web.ResponseWriter) {
 		if w.Written() {
@@ -182,8 +177,8 @@ func (h *ContextHandler) addIDHeaderEndOfRequestFunc(namespace, id string) web.B
 			return
 		}
 
-		headerName := h.Cfg.IDResponseHeaderPrefix + namespaceToHeader[namespace] + "-Id"
-		w.Header().Add(headerName, id)
+		headerName := fmt.Sprintf("%s-Identity-Id", h.Cfg.IDResponseHeaderPrefix)
+		w.Header().Add(headerName, fmt.Sprintf("%s:%s", namespace, id))
 	}
 }
 
